b2a9e991dbec8bfb4ebb6b63a747678d5f99e5d2
Anton Räihä <anton.raiha@wunderdog.fi>
2019-06-22 22:56:56 +0300

    Add prettier & format code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
 // CAUTION!!! This gives this code full read/write rights from/to the Realtime Database.
-import * as admin from 'firebase-admin'
+import * as admin from "firebase-admin";
 
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 
@@ -12,57 +12,73 @@ admin.initializeApp();
 const app = express();
 const main = express();
 
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
-main.use('/', app);
+main.use("/", app);
 main.use(bodyParser.json());
 
 const webApi = functions.https.onRequest(main);
 
-const asyncMiddleware = (fn: any) =>
-  (req: any, res: any, next: any) => {
-    Promise.resolve(fn(req, res, next))
-      .catch(next);
-  };
+const asyncMiddleware = (fn: any) => (req: any, res: any, next: any) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
 
-app.get('/coffee', asyncMiddleware(async (request: Request, response: Response) => {
-    if(request.query.token !== functions.config().wuffice_coffee.slack_command_token) {
-        response.status(401).send({error: "Access denied"})
-        return;
+app.get(
+  "/coffee",
+  asyncMiddleware(async (request: Request, response: Response) => {
+    if (
+      request.query.token !==
+      functions.config().wuffice_coffee.slack_command_token
+    ) {
+      response.status(401).send({ error: "Access denied" });
+      return;
     }
 
-    const snapshot = await admin.database().ref('/timestamped_measures').orderByChild("timestamp").limitToLast(1)
-    .once('value')
+    const snapshot = await admin
+      .database()
+      .ref("/timestamped_measures")
+      .orderByChild("timestamp")
+      .limitToLast(1)
+      .once("value");
 
     snapshot.forEach(childSnapshot => {
-        const {value, timestamp} = childSnapshot.val();
-        if(Date.now()-timestamp > 1000*60*5) {
-            response.send(`Oops! Seems like the temperature hasn't updated in a while. Please fix it :pray: The latest update is from ${new Date(timestamp)}`);
+      const { value, timestamp } = childSnapshot.val();
+      if (Date.now() - timestamp > 1000 * 60 * 5) {
+        response.send(
+          `Oops! Seems like the temperature hasn't updated in a while. Please fix it :pray: The latest update is from ${new Date(
+            timestamp
+          )}`
+        );
+      } else {
+        if (value >= 50) {
+          response.send(
+            `The temperature of the coffee is currently ${parseFloat(
+              value
+            ).toFixed(0)} °C :coffee_parrot: :feelsgoodman:`
+          );
         } else {
-            if(value >= 50) {
-                response.send(`The temperature of the coffee is currently ${parseFloat(value).toFixed(0)} °C :coffee_parrot: :feelsgoodman:`);
-            } else {
-                response.send(`No fresh coffee for you :feelsbadman:`);
-            }
+          response.send(`No fresh coffee for you :feelsbadman:`);
         }
+      }
     });
-}))
+  })
+);
 
-const formatData = functions.database.ref('/coffee1/temp/{pushId}/')
-    .onCreate(
-        (snapshot, context) => {
-            const original = snapshot.val();
-            const pushId = context.params.pushId;
+const formatData = functions.database
+  .ref("/coffee1/temp/{pushId}/")
+  .onCreate((snapshot, context) => {
+    const original = snapshot.val();
+    const pushId = context.params.pushId;
 
-            console.log(`Detected new measure ${original} with pushId ${pushId}`);
+    console.log(`Detected new measure ${original} with pushId ${pushId}`);
 
-            return admin.database().ref('timestamped_measures').push({
-                value: original,
-                timestamp: admin.database.ServerValue.TIMESTAMP
-            });
-        });
+    return admin
+      .database()
+      .ref("timestamped_measures")
+      .push({
+        value: original,
+        timestamp: admin.database.ServerValue.TIMESTAMP
+      });
+  });
 
-export {
-    webApi,
-    formatData
-}
+export { webApi, formatData };
