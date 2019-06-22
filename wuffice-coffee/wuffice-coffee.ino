08bbb0a99b50912c7564484caabd6a9107560861
Anton Räihä <anton.raiha@wunderdog.fi>
2019-06-22 12:28:59 +0300

    Add sketch for microcontroller

---
+++ wuffice-coffee/wuffice-coffee.ino
@@ -0,0 +1,65 @@
+#include <FirebaseESP8266.h>
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+#define FIREBASE_HOST ""
+#define FIREBASE_AUTH ""
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+unsigned long check_wifi = 30000;
+
+FirebaseData firebaseData;
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(1000);
+    digitalWrite(BUILTIN_LED, LOW);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  mlx.begin(); 
+
+  pinMode(BUILTIN_LED, OUTPUT);
+}
+
+void loop()
+{
+
+  if ((WiFi.status() != WL_CONNECTED) && (millis() > check_wifi)) {
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(1000);
+    digitalWrite(BUILTIN_LED, LOW);
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    check_wifi = millis() + 30000;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    float objTemp = mlx.readObjectTempC();
+    
+    Serial.print(objTemp);
+    Serial.println("*C");
+
+    Firebase.pushFloat(firebaseData, "/coffee1/temp", objTemp);
+  }
+}
